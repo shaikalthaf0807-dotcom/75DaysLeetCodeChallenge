0a9c695b5a9fece72b7e4b1552f70fcfbf6a138d
shaikalthaf0807-dotcom <shaikalthaf0807@gmail.com>
2026-03-30 14:46:36 +0530

    Time: 4 ms (41.27%), Space: 43.1 MB (89.71%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }else if(ch==')' || ch==']' || ch=='}'){
+                if(st.isEmpty()){
+                    return false;
+                }
+                char top=st.pop();
+                if(ch==')' && top != '(') return false;
+                if(ch==']' && top != '[') return false;
+                if(ch=='}' && top != '{') return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
